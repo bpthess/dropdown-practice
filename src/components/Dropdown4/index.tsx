f2b8806db212cb7f3c6e2687e4344561b4d3b9d7
bpthess <bpthess@naver.com>
2022-09-29 22:36:24 +0900

    드롭다운4 구현

--- src/components/Dropdown4/index.tsx
+++ src/components/Dropdown4/index.tsx
@@ -1,5 +1,19 @@
+// react-dropdown 라이브러리
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+const options = ["one", "two", "three"];
+
+const defaultOpation = options[0];
+
 const Dropdown4 = () => {
-  return <div>index</div>;
+  return (
+    <Dropdown
+      options={options}
+      value={defaultOpation}
+      placeholder="Select an option"
+    />
+  );
 };
 
 export default Dropdown4;
